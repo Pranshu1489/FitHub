dcebea4203ea4033b241479b4ebb122f8813593d
Pranshu1489 <72308794+Pranshu1489@users.noreply.github.com>
2023-11-02 01:14:43 +0530

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a3a6e27a00mshb4a03d93df0714ap1dea1ajsnbf7a8f29a8ba',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a3a6e27a00mshb4a03d93df0714ap1dea1ajsnbf7a8f29a8ba',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
